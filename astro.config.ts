664d7cb639fce6215e946f3e478fbfa09250f7b0
Thomas Leleko <thomasleleko.de>
2025-10-12 00:40:56 +0200

    Add deploy workflow

--- astro.config.ts
+++ astro.config.ts
@@ -22,6 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
+  site: 'https://tawmae.github.io/',
+  base: '/tnx-site/',
   output: 'static',
 
   integrations: [
